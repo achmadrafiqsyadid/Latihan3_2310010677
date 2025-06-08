1142f3ab358a08bbb7c92a544b0b07a7c60d5a27
achmadrafiqsyadid <bandspacemo@gmail.com>
2025-06-08 22:04:04 +0800

    membuat class matematikaberaksi

--- src/hitung/MatematikaBeraksi.java
+++ src/hitung/MatematikaBeraksi.java
@@ -2,11 +2,11 @@ package hitung;
 
 public class MatematikaBeraksi {
     public static void main(String[] args) {
-        Matematika mtk = new Matematika(10.5, 4.5);
+        Matematika rafiq = new Matematika(7,7);
 
-        System.out.println("Hasil Penjumlahan : " + mtk.setPenjumlahan());
-        System.out.println("Hasil Pengurangan : " + mtk.setPengurangan());
-        System.out.println("Hasil Perkalian   : " + mtk.setPerkalian());
-        System.out.println("Hasil Pembagian   : " + mtk.setPembagian());
+        System.out.println("Hasil Penjumlahan : " + rafiq.setPenjumlahan());
+        System.out.println("Hasil Pengurangan : " + rafiq.setPengurangan());
+        System.out.println("Hasil Perkalian   : " + rafiq.setPerkalian());
+        System.out.println("Hasil Pembagian   : " + rafiq.setPembagian());
     }
 }
