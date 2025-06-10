0bd2ca9ee420eed43666a04fed850edc2aaf0eb9
achmadrafiqsyadid <bandspacemo@gmail.com>
2025-06-10 14:02:23 +0800

    menambahkan atribut

--- src/Latihan3/Rumus.java
+++ src/Latihan3/Rumus.java
@@ -1,13 +1,21 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package Latihan3;
 
-/**
- *
- * @author Dell 5290
- */
 public class Rumus {
-    
+
+    // Atribut bertipe bilangan bulat
+    public int sisi;
+    public int panjang;
+    public int lebar;
+    public int hasil;
+
+    // Contoh konstruktor (opsional)
+    public Rumus() {
+        this.sisi = 0;
+        this.panjang = 0;
+        this.lebar = 0;
+        this.hasil = 0;
+    }
+
+    // Metode lain bisa ditambahkan di sini
 }
