c95acae4d4287319a5b775fa28c171155c5c852e
achmadrafiqsyadid <bandspacemo@gmail.com>
2025-06-10 13:44:35 +0800

    membuat object

---
+++ src/LATIHAN2/DataDiri.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package LATIHAN2;
+
+
+public class Datadiri {
+    private String nama;
+    private String npm;
+    private String kelas;
+
+    // Konstruktor
+    public Datadiri(String nama, String npm, String kelas) {
+        this.nama = nama;
+        this.npm = npm;
+        this.kelas = kelas;
+    }
+
+    // (Optional) Getter untuk melihat isi atribut
+    public String getNama() {
+        return nama;
+    }
+
+    public String getNpm() {
+        return npm;
+    }
+
+    public String getKelas() {
+        return kelas;
+    }
+}
---
+++ src/LATIHAN2/DataDiriBeraksi.java
@@ -0,0 +1,14 @@
+
+package LATIHAN2;
+
+public class DataDiriBeraksi {
+    public static void main(String[] args) {
+        // Membuat objek dari class Datadiri dengan nama objek: rafiq
+        Datadiri rafiq = new Datadiri("Rafiq Alamsyah", "231234567", "TI-2B");
+
+        // Menampilkan data
+        System.out.println("Nama  : " + rafiq.getNama());
+        System.out.println("NPM   : " + rafiq.getNpm());
+        System.out.println("Kelas : " + rafiq.getKelas());
+    }
+}
