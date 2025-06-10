7c82ec8662d663358cb0f7079429eaaff524b468
achmadrafiqsyadid <bandspacemo@gmail.com>
2025-06-10 13:59:03 +0800

    initial init

--- src/LATIHAN2/DataDiriBeraksi.java
+++ src/LATIHAN2/DataDiriBeraksi.java
@@ -4,7 +4,7 @@ package LATIHAN2;
 public class DataDiriBeraksi {
     public static void main(String[] args) {
         // Membuat objek dari class Datadiri dengan nama objek: rafiq
-        Datadiri rafiq = new Datadiri("Rafiq Alamsyah", "231234567", "TI-2B");
+        Datadiri rafiq = new Datadiri("Rafiq ", "2310010677", "TI-4A");
 
         // Menampilkan data
         System.out.println("Nama  : " + rafiq.getNama());
---
+++ src/Latihan3/Rumus.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Latihan3;
+
+/**
+ *
+ * @author Dell 5290
+ */
+public class Rumus {
+    
+}
